357e43be7af780b1d4caa05e1f94c3b5df4c2fc5
Markus Schwarzmann <info@masc-it.com>
2025-03-23 13:20:50 +0100

    Beide FileInputs über einen Filehandler abgebildet.

--- js/files.ts
+++ js/files.ts
@@ -1,36 +1,22 @@
-// $('#btnDB').on('click', function() { 
-//   $('#rulesInput').trigger('click');
-// })
 
-$('body').on('change', '#rulesInput', async function() { 
+
+$('body').on('change', 'input[type=file]', async function() { 
+  const id = this.id;
   const file = this.files[0];
-  const key = 'Regeln';
+  let key = file.name.replace(/\.[^/.]+$/, ''); 
   const text = await file.text();
-  localStorage.setItem(key,text);
-  if (localStorage.getItem(key) == null) {
-    console.log(`Regeln konnten nicht geladen werden`);
+  if (id == 'rulesInput'){
+    if (key == 'datenbank') key = 'Regeln'; else {
+    alert ('Es dürfen nur datenbank.xml Dateien als Regeln geladen werden.');
+    return;}
   } else {
-    console.log(`Regeln wurden geladen`);
-  }
-  location.reload();
-})
-
-$('#btnChar').on('click', function() { 
-  $('#charInput').trigger('click');
-})
-
-$('body').on('change', '#charInput', async function() { 
-    const file = this.files[0];
-    const key = file.name.replace(/\.[^/.]+$/, '');
-    const text = await file.text();
   // Version 8 prüfen
     let xml = $.parseXML(text);
     let CharVersion = $(xml).find("CharakterVersion").text();
+    if (CharVersion == '') {alert('Das ist keine Ilaris-Charakter-Datei und kann somit nicht geladen werden.'); return;} else
     if (CharVersion != '8') alert('App unterstützt Version 8. Charakter hat Version ' + CharVersion +'. Könnte Probleme bereiten.')
-    localStorage.setItem(key,text);
-    for (let [key, value] of Object.entries(localStorage)) {
-    console.log(`${key}`);
   } 
+  localStorage.setItem(key,text);
   location.reload();
 })
 
